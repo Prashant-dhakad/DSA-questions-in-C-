5798a48387ddd90c0de1480c4eddf36128839bcc
Prashant Dhakad <prashantdhakad8001@gmail.com>
2023-02-01 12:00:30 +0530

    2D-ArrayQuestion05 Added 2D-ArrayQuestion05 Added

---
+++ 2D-Array/2D-ArrayQuestion05.cpp
@@ -0,0 +1,41 @@
+//Given a square matrix, turn it by 90 degree in a clockwise direction without usng any extra space.
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void rotateArray(vector<vector<int>> & vec){
+  int n = vec.size();
+  //Transpose
+  for(int i = 0 ; i<n ; i++){
+    for(int j = 0 ; j<i ; j++){
+      swap(vec[i][j], vec[j][i]);
+    }
+  }
+  //Reverse every row.
+  for(int i = 0 ; i<n ; i++){
+    reverse(vec[i].begin(),vec[i].end());
+  }
+  return ;
+}
+
+int main() {
+  int n;
+  cin>>n;
+
+  vector <vector<int>> vec(n,vector<int>(n));
+
+  for(int i = 0 ; i<n ; i++){
+    for(int j = 0 ; j<n ; j++){
+      cin>>vec[i][j];
+    }
+  }
+
+  rotateArray(vec);
+  for(int i = 0; i<n ; i++){
+    for(int j = 0 ; j<n ; j++){
+      cout<<vec[i][j]<<" ";
+    }cout<<endl;
+  }
+  return 0;
+}
